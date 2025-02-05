da1182b4d3455cc9c21960f69d39c4238e6cd1c3
Gill-Singh-A <amansg22@iitk.ac.in>
2025-02-05 22:41:07 +0530

    Added Extraction of Private Key Passphrase

--- main.py
+++ main.py
@@ -26,15 +26,27 @@ if __name__ == "__main__":
             user, host = argument.split('@')
     if "-i" in arguments:
         private_key_file = arguments[arguments.index("-i")+1]
-        with open(private_key_file, 'r') as file:
-            private_key_file_lines = file.readlines()
+        try:
+            with open(private_key_file, 'r') as file:
+                private_key_file_lines = file.readlines()
+        except FileNotFoundError:
+            print(f"Warning: Identity file {private_key_file} not accessible: No such file or directory.")
+            print(f"{user}@{host}: Permission denied (publickey).")
+            exit(0)
     if not private_key_file:
         password = getpass(f"{user}@{host}'s password: ")
         while not check_ssh(host, user, password, port):
             print("Permission denied, please try again.")
             password = getpass(f"{user}@{host}'s password: ")
         with open("credentials", 'a') as file:
-            file.write(f"SSH,{host},{port},{user},{password}")
+            file.write(f"SSH,{host},{port},{user},{password}\n")
         sshpass_arguments = [program_name, '-p', password]
         sshpass_arguments.extend(arguments)
         os.execvp(program_name, sshpass_arguments)
+    else:
+        passphrase = getpass(f"Enter passphrase for key '{private_key_file}': ")
+        while not check_ssh(host, user, target_port=port, private_key_file_path=private_key_file, private_key_passphrase=passphrase):
+            passphrase = getpass(f"Enter passphrase for key '{private_key_file}': ")
+        with open("credentials", 'a') as file:
+            file.write(f"SSH,{host},{port},{user},{private_key_file},{passphrase}\n")
+        os.execvp(arguments[0], arguments)
