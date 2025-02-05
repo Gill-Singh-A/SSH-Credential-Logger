3c3df6bb930984d89c29bd3569d241c631de710a
Gill-Singh-A <amansg22@iitk.ac.in>
2025-02-05 22:15:45 +0530

    Implemented Extraction of Data from Command-Line Arguments

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #! /usr/bin/env python3
 
+from modified_paramiko_pkey import KeyDecrpter
+
+import sys, paramiko
+from getpass import getpass
+
 if __name__ == "__main__":
-    pass
+    arguments = sys.argv[1:]
+    user, host, port, private_key_file = None, None, 22, None
+    if "-p" in arguments:
+        port = arguments[arguments.index("-p")+1]
+    for argument in arguments:
+        if '@' in argument:
+            user, host = argument.split('@')
+    if "-i" in arguments:
+        private_key_file = arguments[arguments.index("-i")+1]
+        with open(private_key_file, 'r') as file:
+            private_key_file_lines = file.readlines()
