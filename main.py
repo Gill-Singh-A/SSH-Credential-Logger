7e03070e9f82be1ea7d6b1f52c056118e3bd2992
Gill-Singh-A <amansg22@iitk.ac.in>
2025-02-05 22:17:18 +0530

    Added Checking SSH Connection Function

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from modified_paramiko_pkey import KeyDecrpter
 import sys, paramiko
 from getpass import getpass
 
+def check_ssh(target, target_user, target_password=None, target_port=22, private_key_file_path=None, private_key_passphrase=None):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        private_key = paramiko.RSAKey.from_private_key_file(private_key_file_path, private_key_passphrase) if private_key_file_path != None else None
+        ssh_client.connect(target, port=target_port, username=target_user, pkey=private_key, look_for_keys=False, allow_agent=False) if private_key != None else ssh_client.connect(target, port=target_port, username=target_user, password=target_password, look_for_keys=False, allow_agent=False)
+        ssh_client.close()
+        return True
+    except Exception as error:
+        return False
+
 if __name__ == "__main__":
     arguments = sys.argv[1:]
     user, host, port, private_key_file = None, None, 22, None
